2d72f84168f0a731ea587a47423cabd6e3e6c09d
Romain Fayolle <romain.fay2@gmail.com>
2019-05-02 10:09:28 +0200

    first commit - init robert

---
+++ bots/robert/actions/increment_question_pro.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const _ = require('lodash');
+
+const increment_question_pro = async () => {
+	const key = 'nb_question_pro';
+	const value = (temp[key] || 0) + 1;
+	temp[key] = value;
+}
+
+return increment_question_pro();
